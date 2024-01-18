f6a8f5ea6dd58f4c4e7ef2e9f94d8f0a743824ab
Naeet <nathnaelmeonnen@gmail.com>
2024-01-18 00:37:39 -0800

    monty

--- pall.c
+++ pall.c
@@ -11,8 +11,11 @@ void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current;
 	(void)line_number;
-
+	
 	current	= *stack;
+
+	if (current == NULL)
+		return;
 	while (current)
 	{
 		printf("%d\n", current->n);
