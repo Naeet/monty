7a04f4c0a3d9fa5c255b4114e139571a1e0f17ae
Naeet <nathnaelmeonnen@gmail.com>
2024-01-18 00:51:17 -0800

    monty

--- execute_instruction.c
+++ execute_instruction.c
@@ -30,23 +30,19 @@ void execute_instruction(char *line, unsigned int line_number, stack_t **stack)
 		push(stack, atoi(arg));
 	}
 	else if (strcmp(opcode, "pall") == 0)
-	{
 		pall(stack, line_number);
-	}
 	else if (strcmp(opcode, "pint") == 0)
-	{
 		pint(stack, line_number);
-	}
 	else if (strcmp(opcode, "pop") == 0)
-	{
 		pop(stack, line_number);
-	}
 	else if (strcmp(opcode, "swap") == 0)
 		swap(stack, line_number);
 	else if (strcmp(opcode, "add") == 0)
 		add(stack, line_number);
 	else if (strcmp(opcode, "nop") == 0)
 		nop(stack, line_number);
+	else if (strcmp(opcode, "sub") == 0)
+		sub(stack, line_number);
 	else
 	{
 		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
